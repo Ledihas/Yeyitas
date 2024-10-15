2935521cae2b7c99f2444eced0c8be1dab9b50c1
Ledihas <chuchua441@gmail.com>
2024-10-15 05:04:41 -0400

    Update settings.py

--- proyectoD/settings.py
+++ proyectoD/settings.py
@@ -107,6 +107,7 @@ DATABASES = {
         default=DATABASE_URL,
         conn_max_age=600
     )
+}
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
