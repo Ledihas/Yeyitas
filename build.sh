e92c2452d59bcd072e1dd6be237a671a8d37f0fa
Ledihas <chuchua441@gmail.com>
2024-10-29 21:05:11 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,12 +2,9 @@
 # Exit on error
 set -o errexit
 
-echo "Instalando pip..."
-apt-get install -y python3-pip
-
 
 # Modify this line as needed for your package manager (pip, poetry, etc.)
-pip3 install -r requirements.txt
+pip install -r requirements.txt
 
 # Convert static asset files
 python manage.py collectstatic --no-input
