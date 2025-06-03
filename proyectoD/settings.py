295f7565fb493e4b6f72a118d1b7ad8cb4848ba5
Ledihas <zahidel.pedroso@gmail.com>
2025-06-03 05:23:29 -0400

    Remove migrations eventos

--- proyectoD/settings.py
+++ proyectoD/settings.py
@@ -50,7 +50,7 @@ except ConnectionFailure as e:
 SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False #'RENDER' not in os.environ
+DEBUG = True #'RENDER' not in os.environ
 
 
 ALLOWED_HOSTS = []
