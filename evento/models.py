e2aad5d4f20e6724709d8b715f757ea0b29b8625
Ledihas <zahidel.pedroso@gmail.com>
2025-06-03 04:11:53 -0400

    Eliminando dependencias innecesarias

--- evento/models.py
+++ evento/models.py
@@ -1,9 +1,9 @@
-from djongo import models
+from django.db import models
 import psycopg2
 #from traitlets import This
 
 class Evento(models.Model):
-    _id = models.ObjectIdField(primary_key=True)
+    _id = models.IntegerField(primary_key=True, auto_created=True)
     nombre = models.CharField(max_length=100)
     descripcion = models.TextField()
     presio = models.DecimalField(max_digits=8, decimal_places=2)
